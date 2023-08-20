a66a6228b56c2ee1c7553968265dae280e986720
galihagussetyawan <galihdevelopment@gmail.com>
2023-08-20 13:25:57 +0700

    feat: create navbar and hero section

---
+++ src/components/Hero.svelte
@@ -0,0 +1,19 @@
+<section class="mx-5 md:mx-24">
+  <h1
+    class="text-[2.9rem] text-center leading-tight mt-5 md:mt-16 md:text-[3.6rem] font-bold"
+  >
+    Maximize <span class="text-ecstasy-600"> Performance Your Instagram </span>
+  </h1>
+  <h2
+    class="md:w-4/5 m-auto py-5 text-[1.2rem] leading-tight font-medium md:text-[1.3rem] text-center text-secondaryTextColor dark:text-secondaryTextColor-dark"
+  >
+    Welcome to the future of Instagram analytics! Unleash your potential with
+    cutting-edge insights, tailored for the true performance enthusiast.
+  </h2>
+  <div class="flex justify-center">
+    <button
+      class="px-5 py-2 rounded-md font-bold text-primaryTextColor text-[1rem] border-2 md:border-4 border-kournikova-500 bg-kournikova-400"
+      >Get Started</button
+    >
+  </div>
+</section>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,51 @@
+<nav
+  class="sticky top-0 px-5 py-2 flex justify-between items-center md:px-24 md:py-2 md:grid md:grid-cols-[10%_70%_20%] bg-colorOnBackground dark:bg-colorOnBackground-dark border-b border-slate-300"
+>
+  <a href="/" class="text-[1.5rem] md:text-3xl font-bold">comein</a>
+  <div
+    class="hidden md:flex justify-center items-center text-[0.9rem] text-thirdTextColor gap-7"
+  >
+    <a
+      href="/about"
+      class="dark:text-thirdTextColor-dark hover:text-primaryTextColor hover:dark:text-primaryTextColor-dark"
+      >About</a
+    >
+    <a
+      href="/service"
+      class="dark:text-thirdTextColor-dark hover:text-primaryTextColor hover:dark:text-primaryTextColor-dark"
+      >Service</a
+    >
+    <a
+      href="/blogs"
+      class="dark:text-thirdTextColor-dark hover:text-primaryTextColor hover:dark:text-primaryTextColor-dark"
+      >Blogs</a
+    >
+    <a
+      href="/contact"
+      class="dark:text-thirdTextColor-dark hover:text-primaryTextColor hover:dark:text-primaryTextColor-dark"
+      >Contact</a
+    >
+  </div>
+  <div class="md:flex md:justify-end gap-5">
+    <button
+      class="hidden md:flex md:px-5 md:py-1 rounded-md border-2 border-kournikova-400 text-[0.9rem] font-medium hover:text-kournikova-400 bg-white"
+      >Get Started</button
+    >
+    <button class="text-thirdTextColor hover:text-secondaryTextColor">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+        />
+      </svg>
+    </button>
+  </div>
+</nav>
