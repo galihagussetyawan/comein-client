54b2462029f4a138f40f1b5bec4ca724d2d68a9b
galihagussetyawan <galihdevelopment@gmail.com>
2023-08-23 13:08:37 +0700

    refactor: change route dashboard

---
+++ src/components/dashboard/Navbar.svelte
@@ -0,0 +1,21 @@
+<nav
+  class="md:h-[6vh] md:px-5 flex justify-between sticky top-0 bg-colorOnBackground dark:bg-colorOnBackground-dark border-b border-slate-300"
+>
+  <a href="/dashboard" class="text-[1.5rem] font-bold">comein</a>
+  <button class="text-thirdTextColor hover:text-secondaryTextColor">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+      />
+    </svg>
+  </button>
+</nav>
---
+++ src/components/dashboard/Sidebar.svelte
@@ -0,0 +1,46 @@
+<aside
+  class="md:w-1/5 md:h-[calc(100vh-6vh)] md:max-h-[calc(100vh-6vh)] md:sticky md:top-[6vh] md:p-5 overflow-hidden overflow-y-visible border-r border-slate-300 bg-backgroundColor dark:bg-backgroundColor-dark"
+>
+  <ul>
+    <li>
+      <a
+        href="/dashboard"
+        class="flex md:p-2 md:hover:bg-indigo-100 md:hover:rounded-md"
+      >
+        <span class="w-full flex items-center gap-1">
+          <svg
+            class="w-6 h-6"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0" />
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <g id="SVGRepo_iconCarrier">
+              <path
+                d="M20.1691 10.2177C20.7024 11.3675 21 12.649 21 13.9999C21 15.4684 20.6483 16.8547 20.0245 18.0793C19.7216 18.674 19.0839 18.9999 18.4165 18.9999H5.58351C4.91613 18.9999 4.27839 18.674 3.97547 18.0793C3.3517 16.8547 3 15.4684 3 13.9999C3 9.02931 7.02944 4.99988 12 4.99988C13.3231 4.99988 14.5795 5.28539 15.711 5.79817M12.7071 13.2929C12.3166 12.9024 11.6834 12.9024 11.2929 13.2929C10.9024 13.6834 10.9024 14.3166 11.2929 14.7071C11.6834 15.0976 12.3166 15.0976 12.7071 14.7071C13.0976 14.3166 13.0976 13.6834 12.7071 13.2929ZM12.7071 13.2929L19.0711 6.92893"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+          </svg>
+          Dashboard
+        </span>
+      </a>
+    </li>
+    <li>
+      <a
+        href="/dashboard/insight"
+        class="flex md:p-2 md:hover:bg-indigo-100 md:hover:rounded-md"
+      >
+        Insight
+      </a>
+    </li>
+  </ul>
+</aside>
