81bf74e388d37eb7487315a023402527f0700ce7
galihagussetyawan <galihdevelopment@gmail.com>
2023-08-25 02:42:24 +0700

    feat: add datepicker range on the dashboard

--- src/components/dashboard/Navbar.svelte
+++ src/components/dashboard/Navbar.svelte
@@ -4,20 +4,33 @@
   <a href="/dashboard" class=" md:m-auto md:ml-0 md:text-[1.5rem] md:font-bold"
     >comein</a
   >
-  <button class="text-thirdTextColor hover:text-secondaryTextColor">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      class="w-6 h-6"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-      />
-    </svg>
-  </button>
+  <div class="md:flex md:items-center md:gap-5">
+    <!-- Period/Range date -->
+    <div class="md:py-1 md:px-3 md:rounded-md md:border bg-white">
+      <p class="md:text-[0.8rem] md:font-light">Range date</p>
+      <p class="md:text-[0.9rem] md:font-medium">
+        <input type="date" class="md:w-28" />
+        -->
+        <input type="date" class="md:w-28" />
+      </p>
+    </div>
+    <!-- END-Period/Range date -->
+
+    <button class="text-thirdTextColor hover:text-secondaryTextColor">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+        />
+      </svg>
+    </button>
+  </div>
 </nav>
