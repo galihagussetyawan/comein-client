6d758150b795042bd310d039f7c1d6b668f5fe16
galihagussetyawan <galihdevelopment@gmail.com>
2023-09-11 14:17:22 +0700

    chore: add package flowbite-svelte

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+  ],
+  plugins: [require("flowbite/plugin")],
   theme: {
     extend: {
       colors: {
