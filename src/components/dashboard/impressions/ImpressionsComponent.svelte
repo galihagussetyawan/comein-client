303081810f8e81363aea5c9a5f63ebe8fb5be849
galihagussetyawan <galihdevelopment@gmail.com>
2023-09-22 01:30:07 +0700

    feat: add impressions charts

---
+++ src/components/dashboard/impressions/ImpressionsComponent.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { periodNameStore } from "../../../store/state";
+  import ImpressionsChartMonth from "./ImpressionsChartMonth.svelte";
+  import ImpressionsChartWeek from "./ImpressionsChartWeek.svelte";
+</script>
+
+{#if $periodNameStore === "last month"}
+  <svelte:component this={ImpressionsChartMonth} />
+{:else if $periodNameStore === "last week"}
+  <svelte:component this={ImpressionsChartWeek} />
+{/if}
