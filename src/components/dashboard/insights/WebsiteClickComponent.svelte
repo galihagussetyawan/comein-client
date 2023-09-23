895a45102bb0704370e6fe8cbc57d15592528b66
galihagussetyawan <galihdevelopment@gmail.com>
2023-09-23 13:52:19 +0700

    feat: add website clicks chart component

---
+++ src/components/dashboard/insights/WebsiteClickComponent.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { periodNameStore } from "../../../store/state";
+  import InsightChartMonth from "./InsightChartMonth.svelte";
+  import InsightChartWeek from "./InsightChartWeek.svelte";
+</script>
+
+{#if $periodNameStore === "last month"}
+  <svelte:component this={InsightChartMonth} name="website_clicks" />
+{:else if $periodNameStore === "last week"}
+  <svelte:component this={InsightChartWeek} name="website_clicks" />
+{/if}
