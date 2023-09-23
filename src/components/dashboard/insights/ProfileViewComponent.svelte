984a73e186dce77d8f35cffff30511919e961a3c
galihagussetyawan <galihdevelopment@gmail.com>
2023-09-23 13:46:12 +0700

    feat: add profile views chart component

---
+++ src/components/dashboard/insights/ProfileViewComponent.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { periodNameStore } from "../../../store/state";
+  import InsightChartMonth from "./InsightChartMonth.svelte";
+  import InsightChartWeek from "./InsightChartWeek.svelte";
+</script>
+
+{#if $periodNameStore === "last month"}
+  <svelte:component this={InsightChartMonth} name="profile_views" />
+{:else if $periodNameStore === "last week"}
+  <svelte:component this={InsightChartWeek} name="profile_views" />
+{/if}
