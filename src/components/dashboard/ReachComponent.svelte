e068d80036ef01e9d3a57a06df40a5b81c5130f0
galihagussetyawan <galihdevelopment@gmail.com>
2023-09-23 13:20:38 +0700

    feat: add reach chart component

---
+++ src/components/dashboard/ReachComponent.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { periodNameStore } from "../../store/state";
+  import InsightChartMonth from "./insights/InsightChartMonth.svelte";
+  import InsightChartWeek from "./insights/InsightChartWeek.svelte";
+</script>
+
+{#if $periodNameStore === "last month"}
+  <svelte:component this={InsightChartMonth} name="reach" />
+{:else if $periodNameStore === "last week"}
+  <svelte:component this={InsightChartWeek} name="reach" />
+{/if}
---
+++ src/components/dashboard/insights/InsightChartMonth.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import {
+    comparisonProfileInsightsStore,
+    periodNameStore,
+    profileInsightsStore,
+  } from "../../../store/state";
+
+  export let name;
+
+  onMount(async () => {
+    const ApexCharts = (await import("apexcharts")).default;
+    if ($periodNameStore === "last month") {
+      const data = $profileInsightsStore.insights.data
+        .find((v) => v.name === name)
+        .values.map((v) => {
+          return {
+            x: new Date(v.end_time).getDate(),
+            y: v.value,
+          };
+        });
+
+      const compData = $comparisonProfileInsightsStore.insights.data
+        .find((v) => v.name === name)
+        .values.map((v) => {
+          return {
+            x: new Date(v.end_time).getDate(),
+            y: v.value,
+          };
+        });
+
+      const options = {
+        series: [
+          {
+            name: "This month",
+            data,
+          },
+          {
+            name: "Last month",
+            data: compData,
+          },
+        ],
+        chart: {
+          height: 350,
+          type: "area",
+          zoom: {
+            enabled: false,
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          curve: "smooth",
+        },
+        fill: {
+          type: "gradient",
+          gradient: {
+            shadeIntensity: 1,
+            opacityFrom: 0.7,
+            opacityTo: 0.9,
+            stops: [0, 90, 100],
+          },
+        },
+      };
+
+      const chart = new ApexCharts(
+        document.querySelector(`#chart-${name}`),
+        options
+      );
+      chart.render();
+    }
+  });
+</script>
+
+<div id={`chart-${name}`} />
