eb771e6086d6302ec66e13e43f17f3386c951ae3
galihagussetyawan <galihdevelopment@gmail.com>
2023-09-25 01:30:57 +0700

    refactor: update link menu sidebar dashboard

--- src/components/dashboard/Sidebar.svelte
+++ src/components/dashboard/Sidebar.svelte
@@ -7,7 +7,7 @@
         href="/dashboard"
         class="flex md:p-2 md:hover:bg-indigo-100 md:hover:rounded-md"
       >
-        <span class="w-full flex items-center gap-1">
+        <span class="w-full flex items-center gap-2">
           <svg
             class="w-6 h-6"
             viewBox="0 0 24 24"
@@ -36,10 +36,87 @@
     </li>
     <li>
       <a
-        href="/dashboard/insight"
+        href="/dashboard/audience"
         class="flex md:p-2 md:hover:bg-indigo-100 md:hover:rounded-md"
       >
-        Insight
+        <span class="w-full flex items-center gap-2">
+          <svg
+            class="w-6 h-6"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0" />
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <g id="SVGRepo_iconCarrier">
+              <path
+                d="M16.719 19.7519L16.0785 14.6279C15.8908 13.1266 14.6146 12 13.1017 12H12H10.8983C9.38538 12 8.10917 13.1266 7.92151 14.6279L7.28101 19.7519C7.1318 20.9456 8.06257 22 9.26556 22H12H14.7344C15.9374 22 16.8682 20.9456 16.719 19.7519Z"
+                stroke="#000000"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <circle
+                cx="12"
+                cy="5"
+                r="3"
+                stroke="#000000"
+                stroke-width="1.5"
+              />
+              <circle cx="4" cy="9" r="2" stroke="#000000" stroke-width="1.5" />
+              <circle
+                cx="20"
+                cy="9"
+                r="2"
+                stroke="#000000"
+                stroke-width="1.5"
+              />
+              <path
+                d="M4 14H3.69425C2.71658 14 1.8822 14.7068 1.72147 15.6712L1.38813 17.6712C1.18496 18.8903 2.12504 20 3.36092 20H7"
+                stroke="#000000"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M20 14H20.3057C21.2834 14 22.1178 14.7068 22.2785 15.6712L22.6119 17.6712C22.815 18.8903 21.8751 20 20.6392 20C19.4775 20 18.0952 20 17 20"
+                stroke="#000000"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+          </svg>
+          Audience
+        </span>
+      </a>
+    </li>
+    <li>
+      <a
+        href="/dashboard/media"
+        class="flex md:p-2 md:hover:bg-indigo-100 md:hover:rounded-md"
+      >
+        <span class="w-full flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+            />
+          </svg>
+          Media
+        </span>
       </a>
     </li>
   </ul>
