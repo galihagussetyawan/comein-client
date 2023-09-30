80b50d60c31553c25d49c9a0e8d2ad6b3536b925
galihagussetyawan <galihdevelopment@gmail.com>
2023-10-01 01:35:03 +0700

    feat: add account and settings page

--- src/components/dashboard/Sidebar.svelte
+++ src/components/dashboard/Sidebar.svelte
@@ -1,5 +1,5 @@
 <aside
-  class="md:w-1/5 md:h-[calc(100vh-10vh)] md:max-h-[calc(100vh-10vh)] md:sticky md:top-[10vh] md:p-5 overflow-hidden overflow-y-visible border-r border-slate-300 bg-backgroundColor dark:bg-backgroundColor-dark"
+  class="md:w-1/5 md:h-[calc(100vh-10vh)] md:max-h-[calc(100vh-10vh)] md:flex md:flex-col justify-between md:sticky md:top-[10vh] md:p-5 overflow-hidden overflow-y-visible border-r border-slate-300 bg-backgroundColor dark:bg-backgroundColor-dark"
 >
   <ul>
     <li>
@@ -120,4 +120,28 @@
       </a>
     </li>
   </ul>
+
+  <!-- Account and Settings menu -->
+  <a
+    href="/dashboard/account"
+    class="max-w-full flex items-center gap-2 md:p-2 md:hover:bg-indigo-100 md:hover:rounded-md"
+  >
+    <span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+        />
+      </svg>
+    </span>
+    Account and Settings
+  </a>
 </aside>
