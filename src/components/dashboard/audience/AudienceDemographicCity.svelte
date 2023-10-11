48669b2436ec5f9d47cb08b0ee1167382d649274
galihagussetyawan <galihdevelopment@gmail.com>
2023-10-11 10:18:23 +0700

    feat: add chart component audience demographic by city

---
+++ src/components/dashboard/audience/AudienceDemographicCity.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { getAudienceDemographics } from "../../../services/account.service";
+
+  export let API_BASE_URL;
+
+  onMount(async () => {
+    const ApexCharts = (await import("apexcharts")).default;
+
+    const res = await getAudienceDemographics(API_BASE_URL, "city");
+    const len = res?.data?.data[0]?.total_value?.breakdowns[0]?.results?.length;
+    const mapping = res?.data?.data[0]?.total_value?.breakdowns[0]?.results
+      .map((v) => {
+        return { x: v?.dimension_values[0], y: v.value };
+      })
+      .sort((a, b) => b.y - a.y)
+      .splice(0, len > 10 ? 10 : len);
+
+    const options = {
+      series: [{ name: "Audience by City", data: mapping }],
+      chart: {
+        height: 350,
+        type: "bar",
+        zoom: {
+          enabled: false,
+        },
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      xaxis: {
+        labels: {
+          maxWidth: 20,
+        },
+      },
+    };
+
+    const chart = new ApexCharts(
+      document.querySelector("#chart-audience-demographic-city"),
+      options
+    );
+    chart.render();
+  });
+</script>
+
+<div id="chart-audience-demographic-city" class="md:min-h-[350px]" />
